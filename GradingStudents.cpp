466898d84716db06c61e0d12a64d0dd2a75d3c31
ExtremeCoder-God <vyelhekar@gmail.com>
2018-08-16 22:05:55 +0530

    Grading Student Algorithum

---
+++ GradingStudents.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the gradingStudents function below.
+ */
+vector<int> gradingStudents(vector<int> grades) {
+    /*
+     * Write your code here.
+     */
+    int len = grades.size(),flag=0,j,i;
+    for(i = 0; i<len; i++){
+        flag=0;
+        if(grades[i]>37){
+            for(j=0; j<3;j++){
+                if((grades[i]+j)%5==0){
+                    flag = 1;
+                    break;
+                }
+            }
+        }
+        if(flag==1){
+            grades[i] += j;
+        }
+    }
+    return grades;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    vector<int> grades(n);
+
+    for (int grades_itr = 0; grades_itr < n; grades_itr++) {
+        int grades_item;
+        cin >> grades_item;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        grades[grades_itr] = grades_item;
+    }
+
+    vector<int> result = gradingStudents(grades);
+
+    for (int result_itr = 0; result_itr < result.size(); result_itr++) {
+        fout << result[result_itr];
+
+        if (result_itr != result.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
